6af96a831a110fbe10d9a2c435a6e090dcf6b1d8
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 07:34:19 +0800

    sidebar 7:34 am

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-64 bg-gray-800 fixed h-full">
+      <div>
+        <h1 className="text-2x text-white font-bold"> Admin</h1>
+      </div>
+      <hr />
+      <ul className="mt-3 text">
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+          <a href="" className="px-3">
+            <faHome className="inline-block w-6 h-6 mr-2 -mt-2">
+              Species Directory
+            </faHome>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
