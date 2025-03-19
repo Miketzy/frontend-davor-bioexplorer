b4f339a6ad1bf83329cc3ef91f7114520dc7fb79
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 07:19:12 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Navbar from "./admin/navbar/Navbar";
+import Sidebar from "./admin/sidebar/Sidebar";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <div>
+        <Sidebar />
+        <div>
+          <Navbar />
+          <div>
+            <h1>Admin Dashboard</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
