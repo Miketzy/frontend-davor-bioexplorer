d1ed12c94cb2d36602521d081175e517d39db8a4
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 07:47:55 +0800

    updated

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -11,7 +11,9 @@ function Sidebar() {
       <ul className="mt-3 text-white font-bold">
         <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
           <a href="" className="px-3">
-            <FaHome className="w-6 h-6">Species Directory</FaHome>
+            <FaHome className="inline-block w-6 h-6 mr-2 -mt-2">
+              Species Directory
+            </FaHome>
           </a>
         </li>
       </ul>
