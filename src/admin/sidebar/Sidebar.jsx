6267303037937ed8d08bb44a19f05cdbb15e20b6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 09:08:25 +0800

    updated

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -10,15 +10,6 @@ import { useState } from "react";
 
 function Sidebar() {
   const [activeIndex, setActiveIndex] = useState(0);
-  const menuItems = [
-    { name: "Species Directory", icon: <DashboardIcon /> },
-    { name: "Add Species", icon: <AddCircleOutlineIcon /> },
-    { name: "List of Species", icon: <AddBoxIcon /> },
-    { name: "Analytics", icon: <AnalyticsIcon /> },
-    { name: "Create Question", icon: <QuizIcon /> },
-    { name: "Gallery", icon: <CollectionsIcon /> },
-    { name: "Contributor Requests", icon: <PeopleIcon /> },
-  ];
   return (
     <div className="w-64 bg-gray-800 fixed h-full px-4 py-2">
       <div className="my-2 mb-4 flex items-center">
@@ -30,21 +21,49 @@ function Sidebar() {
         <h1 className="text-2x text-white font-bold">Admin Dashboard</h1>
       </div>
       <hr className="border-t-2 border-white" />
-      <ul className="text-white font-bold">
-        {menuItems.map((item, index) => (
-          <li
-            key={index}
-            className={`mb-2 rounded py-2 px-3  cursor-pointer transition-all ${
-              activeIndex === index
-                ? "bg-blue-700 shadow-lg"
-                : "hover:bg-blue-500 hover:shadow"
-            }`}
-            onClick={() => setActiveIndex(index)}
-          >
-            <span className="inline-block w-6 h-6 mr-2 -mt-2">{item.icon}</span>
-            {item.name}
-          </li>
-        ))}
+      <ul className="mt-3 text-white font-bold">
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Species Directory
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <AddCircleOutlineIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Add Species
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <AddBoxIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            List of Species
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <AnalyticsIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Analytics
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <QuizIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Create Question
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <CollectionsIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Gallery
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <PeopleIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Contributor Requests
+          </span>
+        </li>
       </ul>
     </div>
   );
