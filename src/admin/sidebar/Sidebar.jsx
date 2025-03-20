889765e21a510288437d1e2f1e0c3c91688abdac
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 08:08:12 +0800

    sidebar 8:08am

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar() {
       </div>
       <hr />
       <ul className="mt-3 text-white font-bold">
-        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
           <a href="" className="px-3">
             <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
             Species Directory
