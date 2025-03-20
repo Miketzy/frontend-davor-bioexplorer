6519cc545084a5e8abe6af0ca87a0ab7201c3046
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 08:21:39 +0800

    sidebar 8:21 am

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -15,6 +15,36 @@ function Sidebar() {
             Species Directory
           </span>
         </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Add Species
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Analytics
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Create Question
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Gallery
+          </span>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2 cursor-pointer">
+          <span className="px-3">
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
+            Contributor Request
+          </span>
+        </li>
       </ul>
     </div>
   );
