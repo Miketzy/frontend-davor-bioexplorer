fb344f36bd4ed48fe4bb21a57be4781f93801e01
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 08:49:42 +0800

    sidebar 8:49am

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -10,7 +10,8 @@ import AddBoxIcon from "@mui/icons-material/AddBox";
 function Sidebar() {
   return (
     <div className="w-64 bg-gray-800 fixed h-full px-4 py-2">
-      <div className="my-2 mb-4">
+      <div className="my-2 mb-4 flex items-center">
+        <img src="/logo.png" alt="Dashboard" className="w-8 h-8 mr-2" />
         <h1 className="text-2x text-white font-bold">Admin Dashboard</h1>
       </div>
       <hr className="border-t-2 border-white" />
