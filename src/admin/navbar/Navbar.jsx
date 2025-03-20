bde20bfc4599204856a2a95321ed2a7e66603a54
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 09:34:28 +0800

    navbar

--- src/admin/navbar/Navbar.jsx
+++ src/admin/navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { CiSearch } from "react-icons/ci";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-gray-800 px-4 flex justify-between">
+      <div className="flex items-center text-xl">
+        <MenuIcon className="text-white me-4 cursor-pointer" />
+        <span className="text-white font-semibold">BioExplorer</span>
+      </div>
+      <div className="flex items-center gap-x-5">
+        <div className="relative md:w-65">
+          <span className="relative md:absolute inset-y-0 left-0 flex items-center pl-2">
+            <button className="p-1 focus:outline-none text-white md:text-black">
+              <CiSearch />
+            </button>
+            <input
+              type="text"
+              className="w-full px-4 py-1 pl-12 rounded shadow outline-none hidden md:block"
+            />
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/home/admin-home/species-directory/Dashboard.jsx
+++ src/home/admin-home/species-directory/Dashboard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Sidebar from "../../../admin/sidebar/Sidebar";
+import Navbar from "../../../admin/navbar/Navbar";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div>
+          <Navbar />
+          <div>
+            <h1>Admin Dashboard</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
