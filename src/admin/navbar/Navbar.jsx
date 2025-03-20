2bba5c6881fc3a9a49ba62aadad49450a7ea03f6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 09:39:37 +0800

    updated

--- src/admin/navbar/Navbar.jsx
+++ src/admin/navbar/Navbar.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
-import { CiSearch } from "react-icons/ci";
+import PageviewIcon from "@mui/icons-material/Pageview";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 px-4 flex justify-between items-center ml-64 h-14">
-      {/* Left Side: Menu Icon & Title */}
+    <nav className="bg-gray-800 px-4 flex justify-between ml-64">
       <div className="flex items-center text-xl">
-        <MenuIcon className="text-white cursor-pointer me-4" />
+        <MenuIcon className="text-white me-4 cursor-pointer" />
         <span className="text-white font-semibold">BioExplorer</span>
       </div>
-
-      {/* Right Side: Search Bar */}
       <div className="flex items-center gap-x-5">
-        <div className="relative md:w-64">
-          <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white md:text-black text-xl cursor-pointer" />
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-full px-10 py-2 rounded shadow outline-none hidden md:block"
-          />
+        <div className="relative md:w-65">
+          <span className="relative md:absolute inset-y-0 left-0 flex items-center pl-2">
+            <button className="p-1 focus:outline-none text-white md:text-black">
+              <PageviewIcon />
+            </button>
+            <input
+              type="text"
+              className="w-full px-4 py-1 pl-12 rounded shadow outline-none hidden md:block"
+            />
+          </span>
         </div>
       </div>
     </nav>
