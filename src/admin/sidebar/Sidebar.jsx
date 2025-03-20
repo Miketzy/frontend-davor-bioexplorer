54198b8894da64480b267fdb354eee1b2922f7ab
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 08:03:24 +0800

    updated

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
+import DashboardIcon from "@mui/icons-material/Dashboard";
 
 function Sidebar() {
   return (
     <div className="w-64 bg-gray-800 fixed h-full">
       <div>
-        <h1 className="text-2x text-white font-bold"> Admin</h1>
+        <h1 className="text-2x text-white font-bold">Admin</h1>
       </div>
       <hr />
       <ul className="mt-3 text-white font-bold">
         <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
           <a href="" className="px-3">
-            <FaHome className="inline-block w-6 h-6 mr-2 -mt-2">
-              Species Directory
-            </FaHome>
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-2" />
+            Species Directory
           </a>
         </li>
       </ul>
