492798a4ec9d1e895ef20b60d32ced49e63a804f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-20 08:05:49 +0800

    updated

--- src/admin/sidebar/Sidebar.jsx
+++ src/admin/sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ function Sidebar() {
       <ul className="mt-3 text-white font-bold">
         <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
           <a href="" className="px-3">
-            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-2" />
+            <DashboardIcon className="inline-block w-6 h-6 mr-2 -mt-1" />
             Species Directory
           </a>
         </li>
